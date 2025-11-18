af7f1e6c7fd262fdb47072742aa17a97409618c6
Joao V <vitortullijoao@gmail.com>
2025-11-18 19:52:22 -0300

    joao: Desafio Aula 09 - Menu SCP

---
+++ aula09/menu.joaov.py
@@ -0,0 +1,15 @@
+# Arquivo: Menu Joao v
+# Autor: Joao Vitor
+# Data 18/11/2025
+
+print("*" * 40)
+print("* SCP- Sistema De Controle De Patio *")
+print(f"Por: Joao V                         ")
+print("*" * 40)
+print(" 1- Cadrastra Bloco ")
+print(" 2-Listar Blocos")
+print(" 3-Listar Blocos ")
+print(" S- Sair")
+print("*" * 40)
+
+input("Precione Entre para continuar...")
