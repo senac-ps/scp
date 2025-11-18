f4c0823a2434e029f34cd06ec2a2471de228514e
Joao V <vitortullijoao@gmail.com>
2025-11-18 20:46:06 -0300

    joaov: calcula m2.py

---
+++ aula10/calcula_m2_joaov.py
@@ -0,0 +1,7 @@
+
+
+
+largura = float(input("Digite a largura (M):   "))
+altura = float(input("Digite altura (m):  "))
+m2 = largura * altura
+print(f"O bloco tem {m2}m2.")
---
+++ aula10/valida_preco_joaov.py
@@ -0,0 +1,6 @@
+preco_m2 = float(input("Digite o preço por m2: "))
+if preco_m2 < 0:            
+    print("Preço inválido!")
+else:
+    print("Preço VALIDO. Calculando.....")
+print("Fim De verificação...")    
