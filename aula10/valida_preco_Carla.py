d2990d058eda1b336fd4cdb77532293c0f37cf47
Beatriz <senacprogamadordesistema>
2025-11-18 21:35:36 -0300

    Carla: Aula 10 Decisões if/elif/else

---
+++ aula10/valida_preco_Carla.py
@@ -0,0 +1,8 @@
+preco_m2 = float(input("Digite o preço por m²:"))
+
+if preco_m2 < 0: 
+      print("ERRO: Preço não pode ser negativo ")
+else:
+      print("Preço VÁLIDO, Calculando...")
+
+print("Fim da verificação.")
---
+++ aula10/valida_qualidade_Carla.py
@@ -0,0 +1,9 @@
+Qualidade = int(input("Digite a Qualidade:"))
+if Qualidade == 1:
+    print("Qualidade selecionada: Primeira")
+elif Qualidade == 2:
+    print("Qualidade selecionada: Segunda ")
+elif Qualidade == 3:
+    print("Qualidade selecionada: Terceira")
+else:
+    print("Qualidade invalída ")
