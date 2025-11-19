a50cf5fd2222f2669b0b874d21f869f2474ebc04
Ian <ian.r.gibara@gmail.com>
2025-11-18 21:28:54 -0300

    ian: valida qualidade.py

--- aula10/valida_qualidade_ian.py
+++ aula10/valida_qualidade_ian.py
@@ -1,1 +1,10 @@
-qualidade = int(input(" Qual a qualidade? "))
+qualidade = int(input(" Qual qualidade?  "))
+if qualidade == 1:
+    print("qualidade boa")
+elif qualidade == 2:
+    print("qualidade regular")
+elif qualidade == 3:
+    print("qualidade ruim")
+else:
+    print("qualidade inválida")
+print("Fim do programa")
