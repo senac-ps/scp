6d3db16dab907894f12a278ae1cd6523936e399e
Beatriz <senacprogamadordesistema>
2025-11-19 21:56:42 -0300

    Carla :Aula 11 loop while

---
+++ aula11/valida_qualidade_v2_Carla.py
@@ -0,0 +1,5 @@
+Qualidade = int(input("Digite a Qualidade 1, 2 ou 3:"))
+while Qualidade < 1 or Qualidade > 3:
+    print("Qualidade invalída ")
+    Qualidade = int(input("Digite novamente a Qualidade:"))
+print("Qualidade valida")
