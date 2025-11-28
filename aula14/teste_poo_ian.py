177b0191b240f045332d164d8c56ee8505a070b1
Ian <ian.r.gibara@gmail.com>
2025-11-27 21:55:09 -0300

    ian: aula 14 poo

---
+++ aula14/teste_poo_ian.py
@@ -0,0 +1,6 @@
+class Bloco:
+    def __init__(self, codigo, preco_m2, largura, altura):
+        self.codigo = codigo
+        self.preco_m2 = preco_m2
+        self.largura = largura
+        self.altura = altura
